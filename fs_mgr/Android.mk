47e84f05080a04119933c2b5d74fad4c011ae370
Wenbo.Zhu <wenbo.zhu_1@nxp.com>
2017-09-26 09:57:03 +0800

    MA-10326 Support DM-Verity on android.o
    Add code to read androidboot.selinux=disabled from command line.
    
    Change-Id: Ic0c284e34b3274d9b7b9876895233a7f6b4a4e74
    Signed-off-by: Wenbo.Zhu <wenbo.zhu_1@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2930
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -35,4 +35,7 @@ LOCAL_STATIC_LIBRARIES := libfs_mgr \
     libselinux
 LOCAL_CXX_STL := libc++_static
 LOCAL_CFLAGS := -Werror
+ifeq ($(DM_VERITY_RUNTIME_CONFIG),true)
+LOCAL_CFLAGS += -DDM_VERITY_DYNAMIC
+endif
 include $(BUILD_EXECUTABLE)
