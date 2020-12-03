cae8287f0a1ad111810acc840bc43c2e7a25ec0e
Ji Luo <ji.luo@nxp.com>
2020-12-03 17:10:47 +0800

    MA-18352-4 Add destroyAttestationIds interface
    Add destroyAttestationIds to delete the provisioned device
    IDs.
    
    Test: delete device ID by destroyAttestationIds.
    
    Change-Id: I4dc23c1c54a9683a7e64711daafb7d1df1187334
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/14193
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/17194
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- trusty/keymaster/4.0/TrustyKeymaster4Device.cpp
+++ trusty/keymaster/4.0/TrustyKeymaster4Device.cpp
@@ -509,7 +509,11 @@ Return<ErrorCode> TrustyKeymaster4Device::deleteAllKeys() {
 }
 
 Return<ErrorCode> TrustyKeymaster4Device::destroyAttestationIds() {
-    return ErrorCode::UNIMPLEMENTED;
+    DestroyAttestationIdsRequest request(impl_->message_version());
+    DestroyAttestationIdsResponse response(impl_->message_version());
+    impl_->DestroyAttestationIds(request, &response);
+
+    return legacy_enum_conversion(response.error);
 }
 
 Return<void> TrustyKeymaster4Device::begin(KeyPurpose purpose, const hidl_vec<uint8_t>& key,
